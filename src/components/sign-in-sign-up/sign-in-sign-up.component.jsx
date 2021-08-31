9b3c1d038b90dfc1a1d8937d27713375d8ef4e83
ginosesia <sesiagino@gmail.com>
2021-08-31 16:22:52 +0100

    SignUp
    Implemented firebase utils, included ability to store authenticated users into firebase database

--- src/components/sign-in-sign-up/sign-in-sign-up.component.jsx
+++ src/components/sign-in-sign-up/sign-in-sign-up.component.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './sign-in-sign-up.styles.scss';
 import SignIn from '../sign-in/sign-in.component';
+import SignUp from '../sign-up/sign-up-component';
 
 const SignInSignUpPage = () => (
     <div className="sign-in-sign-up">
         <SignIn />
+        <SignUp />
     </div>
 );
 
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,5 +1,6 @@
 import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
+import 'firebase/compat/firestore';
 
 firebase.initializeApp({
   apiKey: "AIzaSyCua1n163ltKucpDCmdLh04L8nJgmWkXPs",
@@ -11,8 +12,33 @@ firebase.initializeApp({
   measurementId: "G-Q8PSB16QSF"
 });
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const snapshot = await userRef.get();
+
+  if (!snapshot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
+  }
+
+  return userRef;
+};
+
 export const auth = firebase.auth();
-// export const firestore = firebase.firestore();
+export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({ prompt: 'select_account' });

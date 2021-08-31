e330a4327dad442d4a634d84429a31ee41a3cd27
ginosesia <sesiagino@gmail.com>
2021-08-31 14:35:07 +0100

    Project Fix
    Created a new project and copied all of the files over to get sass working properly again

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,25 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+
+firebase.initializeApp({
+  apiKey: "AIzaSyCua1n163ltKucpDCmdLh04L8nJgmWkXPs",
+  authDomain: "crown-clothing-b7ee5.firebaseapp.com",
+  projectId: "crown-clothing-b7ee5",
+  storageBucket: "crown-clothing-b7ee5.appspot.com",
+  messagingSenderId: "739745720167",
+  appId: "1:739745720167:web:c6aa66f99d33a8a130f1c2",
+  measurementId: "G-Q8PSB16QSF"
+});
+
+export const auth = firebase.auth();
+// export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => {
+  auth.signInWithPopup(provider).then((res) => {
+    console.log(res.user)
+  }).catch((error) => {
+    console.log(error.message)
+  })
+}

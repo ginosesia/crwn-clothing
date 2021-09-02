84ff5ed6b438dc7f7d30182cb39423816556d275
ginosesia <sesiagino@gmail.com>
2021-09-02 13:20:38 +0100

    ShopData & CartItems
    Updated shop data with new data, updated utils to group items in cart reducer

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -3,3 +3,8 @@ import CartAcationTypes from './cart.types';
 export const toggleCartHidden = () => ({
     type: CartAcationTypes.TOGGLE_CART_HIDDEN
 });
+
+export const addItem = item => ({
+    type: CartAcationTypes.ADD_ITEM,
+    payload: item
+});

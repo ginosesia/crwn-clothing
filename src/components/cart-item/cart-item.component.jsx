9143c292fe1cfca83dcd1463531b8676c12b5fed
ginosesia <sesiagino@gmail.com>
2021-09-09 15:55:16 +0100

    Stripe Integration
    updating stripe integration and updating favicon and title

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -13,7 +13,7 @@ const CartItem = ({
         <img src={imageUrl} alt="item" />
         <div className="item-details">
             <span className="name">{name}</span>
-            <span className="price">{ quantity } x £{price}</span>
+            <span className="price">{ quantity } x ${price}</span>
         </div>
     </div>
 );
